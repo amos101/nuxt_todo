caf00ffe20087e708607df91b2465aaaa28cd2a1
amos101 <calcio.vita@gmail.com>
2022-01-10 21:21:36 +0900

    todo追加実装

---
+++ pages/contact.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+      <h1>FormSubmit Demo</h1>
+      <form  action="http://localhost:3000/thankyou" method="POST" onSubmit="check()">
+              <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+              <!-- <input type="hidden" name="_next" value="http://formsubmit.co/calcio.vita@gmail.com"> -->
+              <input type="hidden" name="_subject" value="new email woooo!">
+              <input type="email" name="email"  placeholder="Email Address" required>
+          <textarea placeholder="Your Message" class="form-control" name="message" rows="" required></textarea>
+        <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+      </form>
+    </div>
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default /*defineComponent*/({
+    /*setup() {
+    },
+    */
+    check() {
+      console.log("check", 1)
+
+      const flag = 0;
+
+      if(document.form.name.value == ""){
+          flag = 1;
+      }
+      else if(document.form.email.value == ""){
+          flag = 1;
+      }
+      else if(document.form.message.value == ""){
+          flag = 1;
+      }
+
+      if(flag){
+          sindow.alert('必須項目に未入力があります');
+          return false;
+      }
+      else{
+          return true;
+      }
+
+    }
+
+
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,58 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <h1>FormSubmit Demo</h1>
-      <form target="_blank" action="http://localhost:3000/thankyou" method="POST">
-              <input type="text" name="name" class="form-control" placeholder="Full Name" required>
-              <!-- <input type="hidden" name="_next" value="http://formsubmit.co/calcio.vita@gmail.com"> -->
-              <input type="hidden" name="_subject" value="new email woooo!">
-              <input type="email" name="email"  placeholder="Email Address" required>
-          <textarea placeholder="Your Message" class="form-control" name="message" rows="" required></textarea>
-        <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
-      </form>
+  <section class="container">
+    <h1>Todo App</h1>
+    <p><input type="text" name="content" v-model="content" placeholder="タスクを入力してください"/></p>
+    <div>
+      <td><button class="button button--green" @click="insert">追加</button></td>
+
     </div>
-  </div>
+    <div class="Filter">
+      <button class="button button--gray is-active">全て</button>
+      <button class="button button--gray">作業前</button>
+      <button class="button button--gray">作業中</button>
+      <button class="button button--gray">完了</button>
+    </div>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tr v-for="(todo, index) in todos" :key="index">
+        <td>{{ todo.content }}</td>
+        <td>({{ todo.created }})</td>
+        <td><button class="button">{{ todo.state }}</button></td>
+        <td><button class="button button--delete" @click="remove(item)">削除</button></td>
+      </tr>
+    </table>
+  </section>
 </template>
 
 <script>
+import {mapState} from 'vuex';
+
 export default {
-  name: 'IndexPage'
+  data: function() {
+    return {
+      content: ''
+    }
+  },
+  computed: {
+    ...mapState(['todos'])
+  },
+  methods: {
+    insert: function() {
+      if(this.content != ''){
+        this.$store.commit('insert', {content: this.content});
+        this.content = '';
+      }
+    }
+  },
+  remove: function(todo) {
+    this.$store.commit('remove', todo)
+  }
 }
 </script>
